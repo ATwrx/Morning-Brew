8503c6a6349c8874ac76a21c37d52653da333e8c
Andrew T <act.webdedigns@gmail.com>
2018-01-06 11:30:49 -0600

    weather - date working

--- assets/logic.js
+++ assets/logic.js
@@ -49,84 +49,80 @@ function getDate() {
 
 function getWeather() {
     let api = "https://fcc-weather-api.glitch.me/api/current?";
-    function showPosition() {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                let userLocation = {};
-                userLocation.lat = position.coords.latitude;
-                userLocation.lon = position.coords.longitude;
-                $.ajax(api + $.param(userLocation)).done(function(r) {
-                    let icon = $("<img>");
-                    let result = $("<h4>");
-                    let cTemp = r.main.temp;
-                    let fTemp = Math.floor(cTemp * 1.8 + 32);
-                    let isCelsius = true;
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let userLocation = {};
+            userLocation.lat = position.coords.latitude;
+            userLocation.lon = position.coords.longitude;
+            $.ajax(api + $.param(userLocation)).done(function(r) {
+                let icon = $("<img>");
+                let result = $("<h4>");
+                let cTemp = r.main.temp;
+                let fTemp = Math.floor(cTemp * 1.8 + 32);
+                let isCelsius = true;
 
-                    result.text(fTemp);
-                    icon.attr("src", r.weather[0].icon);
-                    icon.attr("alt", r.weather[0].description);
-                    result.text(r.weather[0].main);
+                result.text(fTemp);
+                icon.attr("src", r.weather[0].icon);
+                icon.attr("alt", r.weather[0].description);
+                result.text(r.weather[0].main);
 
-                    $(".weather").append(result, icon)
-                    $(".temp").click(function() {
-                        if (isCelsius == true) {
-                            tempArea.text(fTemp);
-                            $("#f-temp").addClass("active");
-                            $("#c-temp").removeClass("active");
-                            isCelsius = false;
-                            return isCelsius;
-                        } else {
-                            tempArea.text(cTemp);
-                            $("#c-temp").addClass("active");
-                            $("#f-temp").removeClass("active");
-                            isCelsius = true;
-                            return isCelsius;
-                        };
-                    });
+                $(".weather").append(result, icon)
+                $(".temp").click(function() {
+                    if (isCelsius == true) {
+                        tempArea.text(fTemp);
+                        $("#f-temp").addClass("active");
+                        $("#c-temp").removeClass("active");
+                        isCelsius = false;
+                        return isCelsius;
+                    } else {
+                        tempArea.text(cTemp);
+                        $("#c-temp").addClass("active");
+                        $("#f-temp").removeClass("active");
+                        isCelsius = true;
+                        return isCelsius;
+                    };
                 });
             });
-        } else {
-            console.log("No Browser Support for Geoloaction")
-        }
+        })
+    } else {
+        $(".weather").text("Geolocation is not supported in your browser") 
     }
-
 }
-
-function sports() {
-    let api = "https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=94d15b4fc0ea4ac8a2102b268ac422de";
-    $.ajax(api).done(function(r) {
-        console.log(r.articles[0]);
-        for (let i = 0; i < 5; i++) {
-            let $div = $("<div>");
-            $div.html("<h5 class='sports-title'><strong><a href='"+ r.articles[i].url +"'>" + r.articles[i].title + "</strong></a></h5><p class='sports-text'>" + r.articles[i].description + "</p>")
-            $div.addClass("sports-article-" + i)
-            if (i == 0) {
-                $(".sports").append($div)
-            } else {
-                let prev = i - 1
-                $(".sports-article-" + prev).append($div);
+    function sports() {
+        let api = "https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=94d15b4fc0ea4ac8a2102b268ac422de";
+        $.ajax(api).done(function(r) {
+            console.log(r.articles[0]);
+            for (let i = 0; i < 5; i++) {
+                let $div = $("<div>");
+                $div.html("<h5 class='sports-title'><strong><a href='" + r.articles[i].url + "'>" + r.articles[i].title + "</strong></a></h5><p class='sports-text'>" + r.articles[i].description + "</p>")
+                $div.addClass("sports-article-" + i)
+                if (i == 0) {
+                    $(".sports").append($div)
+                } else {
+                    let prev = i - 1
+                    $(".sports-article-" + prev).append($div);
+                }
             }
-        }
-        let $h3 = $("<h3>").text("Sports News")
-        $h3.addClass("module-title")
-        $(".sports-article-0").prepend($h3)
+            let $h3 = $("<h3>").text("Sports News")
+            $h3.addClass("module-title")
+            $(".sports-article-0").prepend($h3)
+        })
+    }
+    // Button for the sign up page
+    $("#sign-up-button").click(function() {
+        var email = $("#email").val();
+        var password = $("#password").val();
+        firebase.auth().createUserWithEmailAndPassword(email, password).
+        catch (function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        })
     })
-}
-// Button for the sign up page
-$("#sign-up-button").click(function() {
-    var email = $("#email").val();
-    var password = $("#password").val();
-    firebase.auth().createUserWithEmailAndPassword(email, password).
-    catch (function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode == 'auth/weak-password') {
-            alert('The password is too weak.');
-        } else {
-            alert(errorMessage);
-        }
-        console.log(error);
-    })
-})
 
