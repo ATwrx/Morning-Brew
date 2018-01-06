43c20cbacbbb4fadc108a07ac80eb90d544e6432
Andrew T <act.webdedigns@gmail.com>
2018-01-06 11:33:31 -0600

    fixed merge

--- assets/logic.js
+++ assets/logic.js
@@ -85,42 +85,9 @@ function getWeather() {
             });
         })
     } else {
-        $(".weather").text("Geolocation is not supported in your browser") 
+        $(".weather").text("Geolocation is not supported in your browser")
     }
 }
-<<<<<<< HEAD
-    function sports() {
-        let api = "https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=94d15b4fc0ea4ac8a2102b268ac422de";
-        $.ajax(api).done(function(r) {
-            console.log(r.articles[0]);
-            for (let i = 0; i < 5; i++) {
-                let $div = $("<div>");
-                $div.html("<h5 class='sports-title'><strong><a href='" + r.articles[i].url + "'>" + r.articles[i].title + "</strong></a></h5><p class='sports-text'>" + r.articles[i].description + "</p>")
-                $div.addClass("sports-article-" + i)
-                if (i == 0) {
-                    $(".sports").append($div)
-                } else {
-                    let prev = i - 1
-                    $(".sports-article-" + prev).append($div);
-                }
-            }
-            let $h3 = $("<h3>").text("Sports News")
-            $h3.addClass("module-title")
-            $(".sports-article-0").prepend($h3)
-        })
-    }
-    // Button for the sign up page
-    $("#sign-up-button").click(function() {
-        var email = $("#email").val();
-        var password = $("#password").val();
-        firebase.auth().createUserWithEmailAndPassword(email, password).
-        catch (function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            if (errorCode == 'auth/weak-password') {
-                alert('The password is too weak.');
-=======
 
 function sports() {
     let api = "https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=94d15b4fc0ea4ac8a2102b268ac422de";
@@ -128,15 +95,35 @@ function sports() {
         console.log(r.articles[0]);
         for (let i = 0; i < 5; i++) {
             let $div = $("<div>");
-            $div.html("<h5 class='sports-title'><strong><a href='"+ r.articles[i].url +"'style='color: white;'>" + r.articles[i].title + "</strong></a></h5><p class='sports-text'>" + r.articles[i].description + "</p>")
+            $div.html("<h5 class='sports-title'><strong><a href='" + r.articles[i].url + "'>" + r.articles[i].title + "</strong></a></h5><p class='sports-text'>" + r.articles[i].description + "</p>")
             $div.addClass("sports-article-" + i)
             if (i == 0) {
                 $(".sports").append($div)
->>>>>>> c42f01fec5ee3b4145e85ec8e9275fe5fc0ad70c
             } else {
-                alert(errorMessage);
+                let prev = i - 1
+                $(".sports-article-" + prev).append($div);
             }
-            console.log(error);
-        })
+        }
+        let $h3 = $("<h3>").text("Sports News")
+        $h3.addClass("module-title")
+        $(".sports-article-0").prepend($h3)
     })
+}
+// Button for the sign up page
+$("#sign-up-button").click(function() {
+    var email = $("#email").val();
+    var password = $("#password").val();
+    firebase.auth().createUserWithEmailAndPassword(email, password).
+    catch (function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
+    })
+})
 
