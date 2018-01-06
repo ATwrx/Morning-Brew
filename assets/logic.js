f2209092a300b8ebb35d6c9d36c6ce51ed294a14
Alex <miller9902@gmail.com>
2018-01-06 12:36:55 -0600

    sat1235

--- assets/logic.js
+++ assets/logic.js
@@ -10,7 +10,7 @@ firebase.initializeApp(config)
 const database = firebase.database()
 const user = firebase.auth().currentUser
 var name, email, photoUrl, uid, emailVerified
-const defaultModules = ["sports", "foo", "bar"]
+const defaultModules = ["sports", "technews", "bar"]
 if (user != null) {
     name = user.displayName
     email = user.email
@@ -39,6 +39,7 @@ function loadDefault() {
         $(".modules-area").append($div)
     }
     sports()
+    technews()
 }
 
 function getDate() {
@@ -117,6 +118,32 @@ function sports() {
         $(".sports-article-0").prepend($h3)
     })
 }
+
+function technews() {
+    let api = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=94d15b4fc0ea4ac8a2102b268ac422de";
+    $.ajax(api).done(function(r) {
+        console.log(r.articles[0]);
+        for (let i = 0; i < 5; i++) {
+            let $div = $("<div>");
+
+            
+
+           $div.html("<h5 class='technews-title'><strong><a href='" + r.articles[i].url + "'style='font-size: 20px;'>" + r.articles[i].title + "</strong></a></h5><p class='technews-text'>" + r.articles[i].description + "</p>")
+
+            $div.addClass("technews-article-" + i)
+            if (i == 0) {
+                $(".technews").append($div)
+            } else {
+                let prev = i - 1
+                $(".technews-article-" + prev).append($div);
+            }
+        }
+        let $h3 = $("<h3>").text("Tech News")
+        $h3.addClass("module-title")
+        $(".technews-article-0").prepend($h3)
+    })
+}
+
 // Button for the sign up page
 $("#sign-up-button").click(function() {
     var email = $("#email").val();
